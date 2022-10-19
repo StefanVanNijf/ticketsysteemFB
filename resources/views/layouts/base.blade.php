da930d7595fff472f61abc9ad759cae0c6a3ed9f
StefanVanNijf <d276072@edu.rocwb.nl>
2022-10-19 11:01:13 +0200

    Initial commit

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE hmtl>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @include("layouts.header")
+    </head>
+
+    <body>
+
+    </body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,8 @@
+<div class="header">
+    <div class="logo-container">
+        <a href="#"><img src="" alt="Company logo"> </a>
+    </div>
+    <a href="#">Hallo</a>
+    <a href="#">Morguh</a>
+    <a href="https://www.google.com/">Middagh</a>
+</div>
